f61798d78c7816fbb54a6ac05fe8b5f7187ec5c2
Kate Foust <kate.e.foust@gmail.com>
2020-04-26 21:41:52 -0700

    added remaining pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 
-//import NoMatch from "./pages/NoMatch";
+import NoMatch from "./pages/NoMatch";
 import Search from "./pages/Search";
-//import Saved from "./pages/Saved";
+import Saved from "./pages/Saved";
 
 import "./App.css";
 
@@ -21,6 +21,8 @@ class App extends Component {
           <Wrapper>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
+          <Route exact path = "/saved" component= {Saved} />
+          <Route exact path = "/noMatch" component = {NoMatch} />
 
           </Wrapper>
         </div>
---
+++ client/src/pages/Saved.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+//import api
+//import Results from "../components/Results";
+
+class Saved extends Component{
+    state = {
+        savedBooks: [],
+    }
+
+    //componentdidmount
+
+    render(){
+        return(
+            <div className="container">
+                <h2>Saved Books</h2>
+                
+            </div>
+        )
+    }
+}
+
+export default Saved;
